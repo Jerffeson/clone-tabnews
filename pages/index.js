d6babc8220892a6e9bd203aa5b9b0b1252e62fbe
Jerffeson Gonçalves <jerffesongsantos@gmail.com>
2024-12-26 14:07:30 +0000

    ajusta a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Modificação</h1>
+    return <h1>
+        Estou ansioso, que chegue logo o dia de amanhã. 
+        Quero comemorar um dos melhores dias ao lado do meu amor! 
+        Você é muito especial pra mim S2
+        </h1>
 }
 
 export default Home;
